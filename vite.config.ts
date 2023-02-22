8ccd0518aef265bf0acabcc0033e330f2ca9b384
TNSaturday <tnsaturday@gmail.com>
2023-02-22 14:46:24 +0300

    Install happy-dom, update vite.config with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import type { UserConfig as VitestUserConfigInterface } from "vitest/config";
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    environment: "happy-dom",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: vitestConfig.test,
+});
