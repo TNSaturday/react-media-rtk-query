fd9073f362dcf6c759ea333c7f973218622ea63d
TNSaturday <tnsaturday@gmail.com>
2023-02-22 23:01:04 +0300

    Add useAppDispatch, useAppSelector hooks, make request from UserList

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { getUsers } from "../store";
+
 function UserList() {
+  const dispatch = useAppDispatch();
+  const { isLoading, users, error } = useAppSelector((state) => state.users);
+
+  useEffect(() => {
+    void dispatch(getUsers());
+  }, [dispatch]);
   return <div>User List</div>;
 }
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import type { PreloadedState } from "@reduxjs/toolkit";
 import { usersReducer } from "./slice/usersSlice";
+
+const rootReducer = combineReducers({
+  users: usersReducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
 });
+
+export * from "./thunks/getUsers";
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = typeof store.dispatch;
